e626128c721effd602898d9175ce0743987821fa
Rinat <khusainovrm@yandex.ru>
2020-09-22 12:39:10 +0300

    first module

---
+++ lib/components/Result.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="custom-counter">
+    Local data: {{ result }} <br />
+    Plugin options: {{ pluginOptions }}
+  </div>
+</template>
+
+<script>
+export default {
+    data:()=>({
+        result: 42
+    }),
+  computed: {
+    pluginOptions() {
+      return this._customPluginOtions || {}
+    },
+  }
+}
+</script>
